91f6344857399bc6084a51eddf8136a4ec709ec4
saki <sakiranbo39@gamil.com>
2022-06-27 16:20:56 +0900

    first commit

---
+++ src/test/java/com/example/Qa202204sakiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Qa202204sakiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
